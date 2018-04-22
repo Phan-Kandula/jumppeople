2be7de1c0196de2713639d51372385f744f605d9
Hackathon Event <hackathon6@e1z1r1p6.42.us.org>
2018-04-21 22:26:39 -0700

    Main Menu is almost Implemented

--- Main Menu/Start.gd
+++ Main Menu/Start.gd
@@ -1,11 +1,12 @@
-extends Node
+extends TextureRect
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
-func _ready(true):
-	$Player_zombie._ready(true)
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
 	pass
 
 #func _process(delta):
